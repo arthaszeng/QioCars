17acc96de77ff3567986b73c8d9492e3bed634b5
JellyLu <634725546@qq.com>
2017-04-05 23:56:27 +0800

    [LYJ] -profile- add userinfo, show avatar and nick name

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -2,6 +2,20 @@ const AV = require('../../libs/av-weapp-min.js');
 var app = getApp();
 
 Page({
+    data: {
+        src: '',
+        nickName: ''
+    },
     onLoad: function () {
+        console.log("username + " + app.globalData.userInfo.nickName);
+        console.log("username + " + app.globalData.userInfo.avataUrl);
+        this.setBasicUserInfo(app.globalData.userInfo);
+        
+    },
+    setBasicUserInfo(userInfo) {
+        this.setData({
+            nickName:userInfo.nickName,
+            src: userInfo.avatarUrl
+        });
     }
 });
